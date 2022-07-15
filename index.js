1460973b3389afd4711fe1054fee05053d502d3c
Tselepie <36919937+Tselepie@users.noreply.github.com>
2022-07-15 16:00:27 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const exp = require('constants');
+const { request } = require('express');
+const express = require('express');
+const req = require('express/lib/request');
+const { dirname, join } = require('path');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+//const mongoose = require('mongoose');
+const port = 8000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'my_cv')));
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.set('html', path.join(__dirname, 'html'));
+app.set('view engine', 'ejs');
+
+/**
+ * Establishing connection with database
+ */
+/*mongoose.connect('mongodb://localhost:27017/authors').then(() => {
+    console.log("CONNECTION ESTABLISHED!!!");
+}).catch(err => {
+    console.log(err);
+})
+*/
+
+app.get('/', (rq, rs) => {
+    rs.render(path.join(__dirname, '/html/cv.ejs'));
+})
+
+app.listen(port, () => {
+    console.log('Listening to port ', port);
+})
